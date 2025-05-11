346c699b9552cceaa8c4ae5d88cc9df44fa4d22b
Fahad <favorfahad@gmail.com>
2025-05-11 20:06:08 +0300

    Update Q-3.cpp

--- Lab-11/Q-3.cpp
+++ Lab-11/Q-3.cpp
@@ -1,33 +1,21 @@
-#include "iostream"
-#include "exception"
+#include <iostream>
+#include <exception>
 using namespace std;
 
-class InvalidTemperatureException : public exception {  
-    string text;
-    public:
-    InvalidTemperatureException(const string &text) : text(text) {}
-
-    void Error () {
-        cout << text << endl;
-    }
-};
+class InvalidTemperatureException : public exception {};
 
 template<typename X>
 void convertToFahrenheit(X celsius) {
     try {
-        if (celsius < -273.15) throw InvalidTemperatureException("Caught InvalidTemperatureException");
-        else {
-            cout << "Attempting to convert " << celsius << "..." << endl;
-        }
+        if (celsius < -273.15) throw InvalidTemperatureException();
+        cout << "Attempting to convert " << celsius << "C..." << endl;
+    } catch (const InvalidTemperatureException& e) {
+        cout << "Caught InvalidTemperatureException - what(): " << e.what() << endl;
     }
+}
 
-    catch (InvalidTemperatureException &E) {
-        E.Error();
-    }
-};
-
-int main () {
-    double temp = -280;
+int main() {
+    double temp = -300;
     convertToFahrenheit(temp);
     temp = 127;
     convertToFahrenheit(temp);
